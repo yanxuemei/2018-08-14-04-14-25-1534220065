a283b562d0e4b1baaaceb97314709f2469dcce25
yanxuemei <mac@macdeMacBook-Pro-3.local>
2019-05-30 23:45:55 +0800

    0123456789

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+
+const number =[
+    ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+    ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+    ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']
+]
+
+module.exports = function main(str) {
+    const result = [[],[],[]]
+    for(let s of str){
+        result[0].push(number[0][s])
+        result[1].push(number[1][s])
+        result[2].push(number[2][s])
+
+    }
+    return result.map(row =>row.join(' '+'\n').join(''))
+
 };
